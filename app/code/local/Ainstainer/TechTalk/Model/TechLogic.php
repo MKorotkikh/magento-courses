4b19f0821ad9fd2c273c007d334fc8318b2e15d8
Mikhail Korotkikh <korotkikh.ms@gmail.com>
2016-10-30 19:07:13 +0200

    Home task #1. model

---
+++ app/code/local/Ainstainer/TechTalk/Model/TechLogic.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Михаил
+ * Date: 27.10.2016
+ * Time: 14:48
+ */
+
+class Ainstainer_TechTalk_Model_TechLogic extends Mage_Core_Model_Abstract {
+    public function sayHello () {
+        return "HELLO WORLD! (this is model)";
+    }
+}
